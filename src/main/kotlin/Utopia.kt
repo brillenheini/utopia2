a1db3d3aa64ff641f58330dc50e6c5cf9232e0dd
Stefan Schweizer <steve.schweizer@gmail.com>
2018-02-10 17:42:42 +0100

    Naive search implementation

--- src/main/kotlin/Utopia.kt
+++ src/main/kotlin/Utopia.kt
@@ -1,6 +1,8 @@
 import mu.KotlinLogging
+import org.apache.commons.io.IOUtils
 import org.archive.io.warc.WARCReaderFactory
 import java.io.FileInputStream
+import java.nio.charset.Charset
 
 private const val FILE = "data/CC-MAIN-20131204131715-00000-ip-10-33-133-15.ec2.internal.warc.wet.gz"
 private val logger = KotlinLogging.logger {}
@@ -13,7 +15,12 @@ fun main(args: Array<String>) {
 
     for (record in archiveReader) {
         val header = record.header
-        logger.debug(header.url)
+
+        val bytes = IOUtils.toByteArray(record, record.available())
+        val content = String(bytes, Charset.forName("utf-8"))
+        if (content.contains("utopia")) {
+            logger.debug { header.url }
+        }
     }
 
     logger.warn("Utopia Machine stopped")
