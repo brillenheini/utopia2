32c854d06e3783b161e8faf9df26c3b46b6686c6
Stefan Schweizer <steve.schweizer@gmail.com>
2018-02-10 18:07:45 +0100

    Extract snippet

--- src/main/kotlin/Utopia.kt
+++ src/main/kotlin/Utopia.kt
@@ -2,6 +2,7 @@ import mu.KotlinLogging
 import org.apache.commons.io.IOUtils
 import org.archive.io.warc.WARCReaderFactory
 import java.io.FileInputStream
+import java.lang.IllegalArgumentException
 import java.nio.charset.Charset
 
 private const val FILE = "data/CC-MAIN-20131204131715-00000-ip-10-33-133-15.ec2.internal.warc.wet.gz"
@@ -18,10 +19,25 @@ fun main(args: Array<String>) {
 
         val bytes = IOUtils.toByteArray(record, record.available())
         val content = String(bytes, Charset.forName("utf-8"))
-        if (content.contains("utopia")) {
-            logger.debug { header.url }
+        val index = content.indexOf(string = "utopia", ignoreCase = true)
+        if (index != -1) {
+            val snippet = content.snippet(index)
+            logger.debug { "${header.url}: $snippet" }
         }
     }
 
     logger.warn("Utopia Machine stopped")
+}
+
+/**
+ * Return a snippet of this String around [index] constrained by [before] and [after] characters
+ * or this String's limits.
+ */
+private fun String.snippet(index: Int, before: Int = 100, after: Int = 100): String {
+    if (index < 0) {
+        throw IllegalArgumentException("index must be >= 0")
+    }
+    val start = Math.max(index - before, 0)
+    val end = Math.min(index + after, this.length)
+    return this.substring(start, end)
 }
