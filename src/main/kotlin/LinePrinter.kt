5ab2c6b8098ebd54a0f95755ceaa322762a2ae76
Stefan Schweizer <steve.schweizer@gmail.com>
2018-03-04 18:08:22 +0100

    Make intro longer

--- src/main/kotlin/LinePrinter.kt
+++ src/main/kotlin/LinePrinter.kt
@@ -7,7 +7,10 @@ private val logger = KotlinLogging.logger {}
 
 class LinePrinter(private val debug: Boolean) {
 
-    fun printIntro() = print("${BOLD_START}Utopia Machine 2.0\n\n$BOLD_END")
+    fun printIntro() {
+        print("${BOLD_START}Utopia Machine 2.0\n\n$BOLD_END")
+        printSnippet(INTRO_URI, INTRO_SNIPPET)
+    }
 
     fun printSnippet(uri: URI, snippet: String) =
         print("$BOLD_START$uri\n\n$snippet\n\n\n\n\n\n$BOLD_END")
@@ -44,5 +47,12 @@ class LinePrinter(private val debug: Boolean) {
         private const val UNDERLINE_END = "\u001b\u002d\u0000"
 
         private const val DEBUG_LENGTH = 30
+
+        private val INTRO_URI = URI("https://de.wikipedia.org/wiki/Utopie")
+        private val INTRO_SNIPPET = """
+            |Eine Utopie ist der Entwurf einer fiktiven Gesellschaftsordnung, die nicht an zeitgenössische historisch-kulturelle Rahmenbedingungen gebunden ist.
+            |
+            |Der Begriff bezieht sich auf „Nicht-Ort“; aus altgriechisch οὐ- ou- „nicht-“ und τόπος tópos „Ort“. Die mit Utopie beschriebene fiktive Gesellschaftsordnung ist meist positiv. Deshalb handelt es sich in dem Sinne um ein Sprachspiel zwischen Utopie und Eutopie aus εὖ (eu) „gut“ und τόπος. Dagegen bezeichnet die Dystopie die pessimistische Beschreibung einer unethisch negativen Gesellschaftsordnung.
+            """.trimMargin()
     }
 }
