e4e4e72cc4be95a2d9291b7eedff653ed2902c36
Stefan Schweizer <steve.schweizer@gmail.com>
2018-03-17 16:15:22 +0100

    Try underline again and resetting all the time

--- src/main/kotlin/print/Highlighter.kt
+++ src/main/kotlin/print/Highlighter.kt
@@ -1,8 +1,6 @@
 package print
 
 const val INIT = "\u001B\u0040"
-const val BOLD_START = "\u001B\u0045"
-const val BOLD_END = "\u001B\u0046"
 const val UNDERLINE_START = "\u001B\u002D\u0001"
 const val UNDERLINE_END = "\u001B\u002D\u0000"
 
@@ -13,7 +11,7 @@ fun String.highlight(terms: List<String>): String {
     var x = this
     for (term in terms) {
         // $0 is the whole matched term
-        x = x.replace(term.toRegex(RegexOption.IGNORE_CASE), "$BOLD_START$0$BOLD_END")
+        x = x.replace(term.toRegex(RegexOption.IGNORE_CASE), "$UNDERLINE_START$0$UNDERLINE_END")
     }
     return x
 }
